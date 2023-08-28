706b1adff383c04bae82110d5f374de86c41fe1b
faskaa <fazilnb@code.edu.az>
2023-08-28 16:40:49 +0400

    first commit

---
+++ StringCount2/StringCount2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace StringCount2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello word";
+            int vowels = 0;
+            int consonants = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == ' ')
+                {
+                    a.Remove(i);
+                    continue;
+                }
+                if (a[i] == 'a' || a[i] =='e' || a[i] =='i' || a[i] =='o' || a[i] =='u') vowels++;
+                else consonants++;
+
+
+
+            }
+            Console.WriteLine("Your vowels count :" + vowels);
+            Console.WriteLine("Your consonants count :" + consonants);
+
+
+        }
+    }
+}
